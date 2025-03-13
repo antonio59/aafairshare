c22a55dc7af5a676a9719b37d83c3752bb07175d
Antonio Smith <59313725+antonio59@users.noreply.github.com>
2025-03-13 20:37:53 +0000

    Update Supabase types to remove preferences and language fields

--- src/core/types/supabase.types.ts
+++ src/core/types/supabase.types.ts
@@ -294,27 +294,21 @@ export type Database = {
           created_at: string | null
           email: string
           id: string
-          language: string | null
           name: string
-          preferences: Json | null
           updated_at: string | null
         }
         Insert: {
           created_at?: string | null
           email: string
           id?: string
-          language?: string | null
           name: string
-          preferences?: Json | null
           updated_at?: string | null
         }
         Update: {
           created_at?: string | null
           email?: string
           id?: string
-          language?: string | null
           name?: string
-          preferences?: Json | null
           updated_at?: string | null
         }
         Relationships: []
